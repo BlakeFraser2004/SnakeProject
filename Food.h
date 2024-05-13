be3a07f75008636cf4bede6365cc0cde1457286a
BlakeFraser2004 <161100771+BlakeFraser2004@users.noreply.github.com>
2024-05-13 16:55:35 +0930

    Create Food.h

---
+++ Food.h
@@ -0,0 +1,18 @@
+// Food.h
+#ifndef FOOD_H
+#define FOOD_H
+
+#include <SFML/Graphics.hpp>
+
+class Food {
+private:
+    sf::RectangleShape shape;
+
+public:
+    Food(int gridSize);
+    void respawn(int maxWidth, int maxHeight);
+    void draw(sf::RenderWindow& window) const;
+    sf::Vector2f getPosition() const;
+};
+
+#endif // FOOD_H
