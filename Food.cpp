6c2d7c6c451d6feb73ae1702f635f740e348a995
BlakeFraser2004 <161100771+BlakeFraser2004@users.noreply.github.com>
2024-05-15 11:33:42 +0930

    Update Food.cpp

--- Food.cpp
+++ Food.cpp
@@ -1,34 +1,30 @@
-// Food.cpp
 #include "Food.h"
 #include <cstdlib> // For rand()
 #include <ctime>   // For seeding rand()
-#include <iostream>
+#include<iostream>
 
 Food::Food(int gridSize) {
     shape.setSize(sf::Vector2f(static_cast<float>(gridSize), static_cast<float>(gridSize)));
     shape.setFillColor(sf::Color::Red);
-    shape.setPosition(0,0);
+    shape.setPosition(15, 15);
 }
 
-void Food::respawn(int maxWidth, int maxHeight) {
-    int gridSizeX = static_cast<int>(((shape.getSize().x)*5)-1);
-    int gridSizeY = static_cast<int>(((shape.getSize().y)*5)-1);
+void Food::respawn(int gridSize) {
     
-    int x = rand() % (gridSizeX);
-    int y = rand() % (gridSizeY);
-    
+    int x = rand() % gridSize;
+    int y = rand() % gridSize;
 
+    // Ensure x and y are within the range (0, gridSize - 1)
+    x = std::min(std::max(x, 0), gridSize - 1);
+    y = std::min(std::max(y, 0), gridSize - 1);
 
-     std::cout<<"position of gridSizeX is: " <<x<<std::endl;
-      std::cout<<"position of gridSizeX is: " <<y<<std::endl;
-    
-    shape.setPosition(sf::Vector2f(x, y));
+    shape.setPosition(sf::Vector2f(x,y));
 }
-
-
 void Food::draw(sf::RenderWindow& window) const {
+    //std::cout << "Drawing food at position: (" << shape.getPosition().x << ", " << shape.getPosition().y << ")" << std::endl;
     window.draw(shape);
 }
+
 
 sf::Vector2f Food::getPosition() const {
     return shape.getPosition();
