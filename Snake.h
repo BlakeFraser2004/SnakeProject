dfae01be52b07a72c5ce39f03a0885312e66ab0b
BlakeFraser2004 <161100771+BlakeFraser2004@users.noreply.github.com>
2024-05-15 11:35:07 +0930

    Update Snake.h

--- Snake.h
+++ Snake.h
@@ -8,11 +8,11 @@
 class Snake {
 private:
     std::vector<sf::Vector2i> segments;
-    sf:: Vector2i direction;
-    sf:: Color color;
+    sf::Vector2i direction;
+    sf::Color color;
     float speed;
     int gridSize;
-    
+
 public:
     Snake(int gridSize);
     void update();
@@ -20,6 +20,10 @@ public:
     void handleInput(sf::RenderWindow& window);
     void grow();
     sf::Vector2i getHeadPosition();
+    bool checkCollisionWithBorder(const GameBoard& gameBoard);
+    bool checkSelfCollision(const sf::Vector2i& foodPosition);
+    void reset();   
+    int getSize() const;
 };
 
-#endif
+#endif // SNAKE_H
